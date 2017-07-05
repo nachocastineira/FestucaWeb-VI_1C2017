3d6fc76b2ac7bead207a9ff66802bace030b06a3
FernandoCarreño <ferezecarr@gmail.com>
2017-07-04 21:44:50 -0300

    agregue algunas imagenes

--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,13 @@
 // un jquery
+$(document).ready(function(){
+  $("#botonRetiro").click(function(){
+    var textoResumen = $("#textoResumen").value();
+    if(textoResumen == ""){
+      return false;
+    }
+    else {
+
+      return "Realizado el descuento del 10% del producto ya que retiras en la sucursal mas cercana";
+    }
+  });
+});
